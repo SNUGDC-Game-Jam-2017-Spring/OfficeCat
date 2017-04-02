fb0c5abdddbc61b8e8c442a42ea45a346d2eaa02
Song <dydwo1127@gmail.com>
2017-04-02 12:41:57 +0900

    3초동안 일 안하면 isworking = false

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -15,15 +15,31 @@ public class GameController : MonoBehaviour {
     public Transform paperStackResizable;
     public Text main;
 
+    bool isWorking {
+        get { return playingTimeCount < 3; } }
+    float playingTimeCount = 0f;
+    int currentWork;
+
 	void Start ()
     {
         paperStackResizable.localScale = new Vector3(1, Level.currentWorkToDo, 1);
         main.text = "서류들에 앞발 도장을 찍자!\n"+ Level.currentWork + "/" + Level.currentWorkToDo;
+        currentWork = Level.currentWork;
     }
 	
 
 	void Update ()
     {
         main.text = "서류들에 앞발 도장을 찍자!\n" + Level.currentWork + "/" + Level.currentWorkToDo;
+        if (currentWork != Level.currentWork)
+        {
+            playingTimeCount = 0f;
+            Debug.Log("Working");
+            currentWork = Level.currentWork;
+        }
+        else
+        {
+            playingTimeCount += Time.deltaTime;
+        }
     }
 }
